97af82213dcdbd9f7bb352f23bc780f2a1271b5c
Johan Euphrosine <proppy@google.com>
2015-07-21 14:10:42 +0900

    fplbase/demo: reorganize samples directory
    - rename demo/ directory to samples/
    - rename and create directory for each samples
    - move textures and shaders to assets/
    - add comments
    
    Bug: 21711607
    
    Change-Id: I29f41f14e098702fab3f390460c55652a25da1ad

--- samples/triangle/triangle.cpp
+++ samples/triangle/triangle.cpp
@@ -1,8 +1,28 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 #include "precompiled.h"
 #include "fplbase/renderer.h"
 #include "fplbase/input.h"
 #include <cassert>
 
+// Game is a sample that displays a colored triangle.
+//
+// It demonstrates usage of:
+// - fpl::Renderer to load shaders from strings and setup rendering.
+// - fpl::InputSystem to query for exit events and elapsed time.
 class Game {
   static const char *vertexShader;
   static const char *fragmentShader;
